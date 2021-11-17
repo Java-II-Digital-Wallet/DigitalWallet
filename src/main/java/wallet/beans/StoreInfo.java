131b3adad9a88bcedbcc59f0c9ca04fc2acf6ecd
JakeM70 <jakem0640@gmail.com>
2021-11-17 15:44:13 -0600

    added store info

---
+++ src/main/java/wallet/beans/StoreInfo.java
@@ -0,0 +1,31 @@
+package wallet.beans;
+
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author jakem - jrmeyer4
+ * CIS175 - Fall 2021
+ * Nov 17, 2021
+ */
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class StoreInfo {
+	@Id
+	@GeneratedValue
+	private String storeCompanyName;
+	private String address;
+	
+
+}
